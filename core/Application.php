3cff4fcd06334a6277e1d99cf2caf4d4faede118
mursidi4321 <mursidi4321@gmail.com>
2024-03-16 06:03:15 +0700

    menambahkan layout bootstrap 4.5

--- core/Application.php
+++ core/Application.php
@@ -7,10 +7,13 @@ use app\core\Request;
 
 class Application
 {
+    public static string $ROOT_PATH;
     public Router $router;
     public Request $request;
-    public function __construct()
+
+    public function __construct($rootPath)
     {
+        self::$ROOT_PATH = $rootPath;
         $this->request = new Request();
         $this->router = new Router($this->request);
     }
